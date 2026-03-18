0f97145a0f8fa443cf859870110a4afe7a9e452c
zulgrl <zulgerel@motforex.com>
2026-03-18 17:02:32 +0800

    news section

--- src/app/_components/Article-Card.tsx
+++ src/app/_components/Article-Card.tsx
@@ -11,23 +11,23 @@ interface ArticleCardProps {
   slug: string;
 }
 
-export function ArticleCard({ image }: ArticleCardProps) {
+export function ArticleCard({ image, title, description, slug }: ArticleCardProps) {
   const { locale, t } = useLocale();
   return (
     <article className="flex h-50 bg-card rounded-lg  mt-10 border-border overflow-hidden shadow-sm hover:shadow-md transition-shadow">
-      <div className="relative w-30/100 aspect-[26/10] overflow-hidden">
-        <Image src={image} alt={"title"} fill className="object-cover" />
+      <div className="relative w-30/100 aspect-26/10 overflow-hidden">
+        <Image src={image} alt={t(title)} fill className="object-cover" />
       </div>
       <div className="flex flex-col flex-1 p-4 w-70/100">
         <h3 className="text-base font-semibold text-foreground leading-snug mb-2 line-clamp-2">
-          {t("userPage.course.title")}
+          {t(title)}
         </h3>
         <p className="text-sm text-muted-foreground leading-relaxed flex-1 mb-4 line-clamp-3">
-          {t("userPage.course.description")}
+          {t(description)}
         </p>
         <div className="flex justify-center">
           <Link
-            href={`/${locale}/user`}
+            href={`/${locale}/user/${slug}`}
             className="px-6 py-2 border border-foreground text-foreground text-xs font-medium rounded-sm hover:bg-foreground hover:text-background transition-colors cursor-pointer"
           >
             {t("services.readMore")}
--- src/app/_components/Article-Section.tsx
+++ src/app/_components/Article-Section.tsx
@@ -18,9 +18,9 @@ export default function ArticleSection() {
 
           {/* News cards grid */}
           <div className="flex-1  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {newsItems.map((item, index) => (
+            {newsItems.map((item) => (
               <ArticleCard
-                key={index}
+                key={item.slug}
                 image={item.image}
                 title={item.titleKey}
                 description={item.descriptionKey}
